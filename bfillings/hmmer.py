a8b4125e598b2d5b6e169f214d947e5b075bc8f0
Jenya Kopylova <jenya.kopylov@gmail.com>
2015-02-04 10:36:09 -0700

    app controller for nhmmer command of HMMER

--- bfillings/hmmer.py
+++ bfillings/hmmer.py
@@ -8,70 +8,47 @@
 
 """ Application controller for HMMER v. 3.1b1 """
 
-from os.path import abspath, join, dirname
+from skbio.parse.sequences import parse_fasta
 
 from burrito.parameters import ValuedParameter, FlagParameter
-from burrito.util import (CommandLineApplication, ResultPath,
-                          ApplicationError)
+from burrito.util import (CommandLineApplication, ResultPath)
 
 
 class hmmer(CommandLineApplication):
     """ HMMER generic application controller.
 
-    	Instead of instantiating this class, instantiate a
-    	subclass for each binary. Available subclasses are:
+        Instead of instantiating this class, instantiate a
+        subclass for each binary. Available subclasses are:
 
-    	hmmer_nhmmer: DNA homology search with profile HMMs
+        hmmer_nhmmer: DNA homology search with profile HMMs
     """
 
     _suppress_stdout = False
     _suppress_stderr = False
 
-    def _input_as_parameters(self, data):
-        """ Set the input path (a fasta filepath)
-        """
-        # The list of values which can be passed on a per-run basis
-        allowed_values = ['-o', '--noali', '--tblout',
-                          '--acc', '-E', '--cpu']
-
-        unsupported_parameters = set(data.keys()) - set(allowed_values)
-        if unsupported_parameters:
-            raise ApplicationError(
-                "Unsupported parameter(s) passed when calling nhmmer: %s" %
-                ' '.join(unsupported_parameters))
-
-        for v in allowed_values:
-            # turn the parameter off so subsequent runs are not
-            # affected by parameter settings from previous runs
-            self.Parameters[v].off()
-            if v in data:
-                # turn the parameter on if specified by the user
-                self.Parameters[v].on(data[v])
-
-        return ''
-
     def getHelp(self):
-	    """ Method that points to documentation """
-	    help_str = """
-	    HMMER package is hosted at:
-	    http://hmmer.janelia.org/software
+        """ Method that points to documentation """
+        help_str = """
+        HMMER package is hosted at:
+        http://hmmer.janelia.org/software
 
-	    The following papers should be cited if this resource is used:
+        The following papers should be cited if this resource is used:
 
-	    [1] Finn R.D., Clements J. and Eddy S.R., "HMMER web server:
-	        interactive sequence similarity searching", Nucl Acids Res,
-	        2011, Web Server Issue 39:W29-W37.
-	    """
-	    return help_str
+        [1] Finn R.D., Clements J. and Eddy S.R., "HMMER web server:
+            interactive sequence similarity searching", Nucl Acids Res,
+            2011, Web Server Issue 39:W29-W37.
+        """
+        return help_str
 
 
 class hmmer_nhmmer(hmmer):
-	""" Controls the nhmmer command of HMMER.
-		nhmmer implements DNA homology search with profile HMMs.
-	"""
+    """ Controls the nhmmer command of HMMER.
+        nhmmer implements DNA homology search with profile HMMs.
+    """
 
-    _command_ = 'nhmmer'
-    _parameters_ = {
+    _command = 'nhmmer'
+    _input_handler = '_input_as_string'
+    _parameters = {
         # Output results to specified file
         '-o': ValuedParameter('-', Name='o', Delimiter=' ',
                               IsPath=True),
@@ -87,7 +64,7 @@ class hmmer_nhmmer(hmmer):
                               IsPath=False, Value=1e-5),
         # Number of parallel CPU working to use for multithreads
         '--cpu': ValuedParameter('--', Name='cpu', Delimiter=' ',
-        		                 IsPath=False, Value=1)
+                                 IsPath=False, Value=1)
     }
 
     def _get_result_paths(self, data):
@@ -96,7 +73,7 @@ class hmmer_nhmmer(hmmer):
         result = {}
 
         # Direct output to file
-        result['FastaOutput'] = ResultPath(
+        result['AlignmentsOutput'] = ResultPath(
             Path=self.Parameters['-o'].Value,
             IsWritten=self.Parameters['-o'].isOn())
 
@@ -104,6 +81,8 @@ class hmmer_nhmmer(hmmer):
         result['TableOutput'] = ResultPath(
             Path=self.Parameters['--tblout'].Value,
             IsWritten=self.Parameters['--tblout'].isOn())
+
+        return result
 
     def getHelp(self):
         """ Method that points to documentation """
@@ -120,15 +99,96 @@ class hmmer_nhmmer(hmmer):
 
 
 def run_nhmmer(fasta_filepath,
-			   profile_hmm,
-			   output_filepath=None,):
-	""" Search a DNA model or alignment against a
-	    DNA database
+               profile_hmm,
+               output_filepath,
+               tblout_filepath=None,
+               report_artifacts=False,
+               noali=True,
+               acc=True,
+               evalue=None,
+               threads=1,
+               HALT_EXEC=False):
+    """ Search a DNA model or alignment against a
+        DNA database
 
-	    Parameters
-	    ----------
+        Parameters
+        ----------
+        fasta_filepath : string
+            filepath to FASTA sequences to search against
+            profile HMM
+        profile_hmm : string
+            filepath to HMM profile
+        output_filepath : string
+            filepath to output alignments
+        tblout_filepath : string, optional
+            filepath to parseable table of hits
+        report_artifacts : boolean, optional
+            if true, return a list of artifact labels
+        noali : boolean, optional
+            if false, do not output alignments
+        acc : boolean, optional
+            if true, prefer accessions over names in output
+        evalue : float, optional
+            E-value threshold for reporting matching sequences
+        threads : integer, optional
+            number of threads to use
 
-	    Return
-	    ------
-	"""
+        Return
+        ------
+        app_result : dict
+            dictionary of open file objects
+        artifacts : list
+            list containing artifact labels (can be empty)
+    """
+    # Instantiate the object
+    nhmmer = hmmer_nhmmer(HALT_EXEC=HALT_EXEC)
 
+    # Set parameters
+    if not noali:
+        nhmmer.Parameters['--noali'].off()
+    if not acc:
+        nhmmer.Parameters['--acc'].off()
+    if evalue:
+        nhmmer.Parameters['-E'].on(evalue)
+    if threads > 0:
+        nhmmer.Parameters['--cpu'].on(threads)
+    else:
+        raise ValueError("Number of threads must be a positive integer.")
+    if tblout_filepath:
+        nhmmer.Parameters['--tblout'].on(tblout_filepath)
+
+    # artifacts can be deduced using the default output
+    # (set by '-o') but the file is more difficult to parse
+    # than '--tblout' and contains more information than
+    # we need for this step
+    if (report_artifacts and tblout_filepath is None):
+        raise ValueError("A table of parseable table of hits is required "
+                         "as output in order to deduce the artifacts.")
+
+    nhmmer.Parameters['-o'].on(output_filepath)
+
+    data = "%s %s" % (profile_hmm, fasta_filepath)
+    app_result = nhmmer(data)
+
+    artifacts = []
+
+    # Find all artifacts
+    if report_artifacts:
+        # Get list of all hits
+        hits = set()
+        for line in app_result['TableOutput']:
+            if line.startswith('#'):
+                continue
+            line = line.strip().split()
+            hits.add(line[0])
+
+        # Get list of all input sequences
+        all_seqs = set()
+        with open(fasta_filepath, 'U') as fasta_f:
+            for label, seq in parse_fasta(fasta_f):
+                all_seqs.add(label)
+
+        # Get sequences (artifacts) which didn't match to the HMM
+        artifacts = list(all_seqs - hits)
+
+    return app_result, artifacts
